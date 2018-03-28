f2ed2e8d6eecc857013ebefab7b03693fd34a292
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-03-28 15:18:34 +0000

    Update with_any_zeal to config

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'with_any_zeal.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'with_any_zeal.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
