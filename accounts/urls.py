0388323837701ed54d8ac30b12a8299e832c9d56
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-01 14:46:02 +0000

    Add accounts urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
+
+app_name = 'accounts'
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='accounts/index.html'), name="index")
 ]
