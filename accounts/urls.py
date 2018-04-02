9ddf9a4b31d79e1e51ed7d5787ff00822013f3a8
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-02 07:13:49 +0000

    Add signup url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.views.generic.base import TemplateView
 
+from accounts import views
+
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='accounts/index.html'), name="index")
+    path('signup/', views.SignUpView.as_view(), name="signup"),
+
+    path('', TemplateView.as_view(template_name='accounts/index.html'), name="index"),
 ]
