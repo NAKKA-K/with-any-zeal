a9ba8e65c1ea64377c38fc28bc85220008b6ab91
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-04 09:30:38 +0000

    Fix import from of Event model & Add context name

--- product/views.py
+++ product/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from product.views import Event
+from product.models import Event
 
 
 # Create your views here.
@@ -11,5 +11,6 @@ class EventListView(ListView):
 
     model = Event
     template_name = 'event_list.hmtl'
+    context_object_name = 'events'
 
 
