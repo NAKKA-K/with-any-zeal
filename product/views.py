598ac0316a6bc0cab7028a90a5cfc808175803c1
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-04 14:07:16 +0000

    Add event create view

--- product/views.py
+++ product/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic import CreateView
 
 from product.models import Event
 
@@ -7,10 +9,21 @@ from product.models import Event
 # Create your views here.
 
 class EventListView(ListView):
-    """ List of Event view """
+    """ List of Event model """
 
     model = Event
-    template_name = 'event_list.hmtl'
+    template_name = 'product/event_list.hmtl'
     context_object_name = 'events'
 
 
+class EventCreateView(CreateView):
+    """ Create of Event model """
+
+    model = Event
+    fields = ('name', 'description', 'readme')
+    template_name = 'product/event_form.html'
+    success_url = reverse_lazy('product:event_list')
+
+    def form_valid(self, form):
+        form.instance.create_user = self.request.user
+        return super().form_valid(form)
