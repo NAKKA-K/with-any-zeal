74d4153442c2a3f4e5ad31ccfb12441e537257b9
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-04 08:56:21 +0000

    Add URL to product

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('product/', include('product.urls')),
 ]
