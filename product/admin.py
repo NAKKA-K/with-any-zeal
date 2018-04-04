d4f80802b2fd17b59371f3f65da8db8face50d33
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-04 09:32:51 +0000

    Add models to admin site

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from product.models import Event
+from product.models import EventJoin
+from product.models import EventSkillTag
+
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(EventJoin)
+admin.site.register(EventSkillTag)
+
