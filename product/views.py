56843703ad7401d2bb59c9fb35d5b596ed9d0a25
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-04 09:05:18 +0000

    Add EventListView

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from product.views import Event
+
 
 # Create your views here.
+
+class EventListView(ListView):
+    """ List of Event view """
+
+    model = Event
+    template_name = 'event_list.hmtl'
+
+
