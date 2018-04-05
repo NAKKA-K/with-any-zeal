6525e35411a213a17c6a0a16a7859ac2d85375c7
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-05 04:54:17 +0000

    Add url to event detail

--- product/urls.py
+++ product/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.EventListView.as_view(), name="event_list"),
     path('create/', views.EventCreateView.as_view(), name="event_create"),
     path('<int:pk>/update/', views.EventUpdateView.as_view(), name="event_update"),
+    path('<int:pk>/detail/', views.EventDetailView.as_view(), name="event_detail"),
 ]
 
