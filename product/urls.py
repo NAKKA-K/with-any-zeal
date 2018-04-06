3cc9b61e32fb8e5d23b9f37509606244f90ec639
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-06 06:53:34 +0000

    Add url to event delete

--- product/urls.py
+++ product/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('create/', views.EventCreateView.as_view(), name="event_create"),
     path('<int:pk>/update/', views.EventUpdateView.as_view(), name="event_update"),
     path('<int:pk>/detail/', views.EventDetailView.as_view(), name="event_detail"),
+    path('<int:pk>/delete/', views.EventDeleteView.as_view(), name="event_delete"),
 ]
 
