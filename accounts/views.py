a357e36bc49f384f32286ab8104dec1575641604
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-09 20:34:02 +0900

    Add mypage view

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic import TemplateView
 
 # Create your views here.
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+class MypageView(TemplateView):
+    template_name = 'accounts/mypage.html'
