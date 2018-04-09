8392ca30e3563c1b2b4e1e18bf3aa5b9ab948c23
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-09 23:57:44 +0900

    Add url to user profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name="signup"),
     path('mypage/', views.MypageView.as_view(), name="mypage"),
-
+    path('<slug:user_name>/', views.ProfileView.as_view(), name="profile"),
 ]
