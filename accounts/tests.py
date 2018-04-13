f3c86d9cacc24d538718b493b4246f7aa0ea3c5b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-04-13 11:25:17 +0900

    Add test of mypage access

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,23 +5,27 @@ from django.contrib.auth import get_user_model
 # Create your tests here.
 class AccounUrlAccessTest(TestCase):
     def setUp(self):
-        pass
+        self.myuser = get_user_model().objects.create(username = 'testsan', password = 'password3')
 
     def test_signup_access(self):
-        client = self.client
-        response = client.get(reverse('accounts:signup'))
+        response = self.client.get(reverse('accounts:signup'))
         self.assertEqual(response.status_code, 200)
 
-        response = client.post(reverse('accounts:signup'), {
+        response = self.client.post(reverse('accounts:signup'), {
             'username':'test_user',
             'password1':'test1234',
             'password2':'test1234',
         })
         self.assertEqual(response.status_code, 302) # Redirect to login if success
-        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all().count(), 2)
 
     def test_mypage_access(self):
-        pass
+        response = self.client.get(reverse('accounts:mypage'))
+        self.assertEqual(response.status_code, 302)
+
+        self.client.force_login(self.myuser)
+        response = self.client.get(reverse('accounts:mypage'))
+        self.assertEqual(response.status_code, 200)
 
     def test_profile_access(self):
         pass
