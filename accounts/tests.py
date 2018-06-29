6f0fe70cd9e4494e3ba9cfa11e0c4595638fbb83
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2018-06-29 00:59:32 +0000

    Update detail test

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,10 +5,13 @@ from django.contrib.auth import get_user_model
 # Create your tests here.
 class AccounUrlAccessTest(TestCase):
     def setUp(self):
-        self.myuser = get_user_model().objects.create(username = 'testsan', password = 'password3')
+        self.myuser = get_user_model().objects.create(
+                username = 'testsan',
+                password = 'password3')
 
     def test_signup_get_request(self):
         response = self.client.get(reverse('accounts:signup'))
+        self.assertTemplateUsed(response, 'accounts/signup.html')
         self.assertEqual(response.status_code, 200)
 
     def test_signup_post_request(self):
@@ -16,21 +19,29 @@ class AccounUrlAccessTest(TestCase):
             'username':'test_user',
             'password1':'test1234',
             'password2':'test1234',
-        })
-        self.assertEqual(response.status_code, 302) # Redirect to login if success
+        }, follow=True)
+        self.assertEqual(response.status_code, 200) # Redirect to login if success
+        self.assertRedirects(response, reverse('login') + '')
+        self.assertTemplateUsed(response, 'registration/login.html')
         self.assertEqual(get_user_model().objects.all().count(), 2)
 
     def test_not_logged_in_mypage_access(self):
-        response = self.client.get(reverse('accounts:mypage'))
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('accounts:mypage'), follow=True)
+        self.assertRedirects(response, reverse('login') + '?next=/accounts/mypage/')
+        self.assertTemplateUsed(response, 'registration/login.html')
+        self.assertEqual(response.status_code, 200)
 
     def test_logged_in_mypage_access(self):
         self.client.force_login(self.myuser)
         response = self.client.get(reverse('accounts:mypage'))
+        self.assertTemplateUsed(response, 'accounts/mypage.html')
         self.assertEqual(response.status_code, 200)
 
     def test_exists_user_profile_access(self):
-        response = self.client.get(reverse('accounts:profile', kwargs = {'user_name': self.myuser.username}))
+        response = self.client.get(
+                reverse('accounts:profile',
+                        kwargs = {'user_name': self.myuser.username}))
+        self.assertTemplateUsed(response, 'accounts/profile.html')
         self.assertEqual(response.status_code, 200)
 
     def test_not_exists_user_profile_access(self):
